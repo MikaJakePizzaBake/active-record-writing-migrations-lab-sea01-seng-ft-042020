8cec071d344c0edb137e1c30f474ec54be5d999d
Trevor Low <tlow94@uw.edu>
2020-05-01 21:55:21 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :student, [:grade, :birthdate] :integer, :string
+    add_column :student, [:grade, :birthdate] [:integer, :string]
   end
 end
